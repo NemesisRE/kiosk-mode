ea1416ed3fbea0105587d0bdbb2cd425524c841e
Iván Pereira Muñoz <elchininet@gmail.com>
2023-12-05 15:27:52 +0100

    Reduce the events management and DOM query logic

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,30 @@
 import ts from 'rollup-plugin-ts';
 import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default [
-	{
-		plugins: [
-			json(),
-			ts({
-				browserslist: false
-			}),
-			terser({
-				output: {
-					comments: false
-				}
-			})
-		],
-		input: 'src/kiosk-mode.ts',
-		output: [
-			{
-				file: 'dist/kiosk-mode.js',
-				format: 'iife'
-			},
-			{
-				file: '.hass/config/www/kiosk-mode.js',
-				format: 'iife'
+export default {
+	plugins: [
+		nodeResolve(),
+		json(),
+		ts({
+			browserslist: false
+		}),
+		terser({
+			output: {
+				comments: false
 			}
-		]
-	}
-];
+		})
+	],
+	input: 'src/kiosk-mode.ts',
+	output: [
+		{
+			file: 'dist/kiosk-mode.js',
+			format: 'iife'
+		},
+		{
+			file: '.hass/config/www/kiosk-mode.js',
+			format: 'iife'
+		}
+	]
+};
