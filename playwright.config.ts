e547ebae27e2796e453716244aa4e94b96cbc17b
Iván Pereira Muñoz <elchininet@gmail.com>
2023-12-17 01:47:18 +0100

    Remove Firefox and Safari from the end-to-end tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,14 +60,14 @@ export default defineConfig({
 				}
 			},
 		},
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] },
-		},
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] },
-		},
+		// {
+		// 	name: 'firefox',
+		// 	use: { ...devices['Desktop Firefox'] },
+		// },
+		// {
+		// 	name: 'webkit',
+		// 	use: { ...devices['Desktop Safari'] },
+		// },
 		/* Test against mobile viewports. */
 		// {
 		//   name: 'Mobile Chrome',
