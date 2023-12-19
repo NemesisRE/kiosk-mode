98ca2b7db8fb5292b224198713a51c263881821f
Iván Pereira Muñoz <elchininet@gmail.com>
2023-12-19 20:44:44 +0100

    Start to collect code coverage

--- tests/url-parameters.spec.ts
+++ tests/url-parameters.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from 'playwright-test-coverage';
 import { URL_PARAMS, SELECTORS } from './constants';
 import { getUrlWithParam } from './utils';
 
